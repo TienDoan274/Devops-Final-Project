0dcfbc2cfd07db6800fe739035d6c8b5c1c67792
TienDoan274 <doannhattien4@gmail.com>
2024-12-22 21:02:43 +0700

    a

--- user-service/application/__init__.py
+++ user-service/application/__init__.py
@@ -1,36 +1,29 @@
+# application/__init__.py
+import config
+import os
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-from prometheus_client import make_wsgi_app
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
-from werkzeug.serving import run_simple
+from prometheus_flask_exporter import PrometheusMetrics
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 
+
 def create_app():
     app = Flask(__name__)
-    
-    # Cấu hình database và các thông số khác
-    app.config['SECRET_KEY'] = 'secret-key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://cloudacademy:cloudacademy@user-db:3306/user'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    metrics = PrometheusMetrics(app)
+
+    metrics.info(f'user_info', f'user API info', version='1.0.0')
+
+    environment_configuration = os.environ['CONFIGURATION_SETUP']
+    app.config.from_object(environment_configuration)
 
     db.init_app(app)
     login_manager.init_app(app)
-    
-    # Import và đăng ký blueprint
-    from .user_api import user_api_blueprint
-    app.register_blueprint(user_api_blueprint)
-    
-    # Add prometheus wsgi middleware
-    app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
-        '/metrics': make_wsgi_app()
-    })
 
-    return app
-
-if __name__ == '__main__':
-    app = create_app()
-    # run_simple là optional, bạn có thể dùng app.run() nếu trong development
-    run_simple('0.0.0.0', 5000, app)
+    with app.app_context():
+        # Register blueprints
+        from .user_api import user_api_blueprint
+        app.register_blueprint(user_api_blueprint)
+        return app
